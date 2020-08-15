64a3e578daa3675a25d877aaf6af79407cb5f88f
Jason Perez <jasandper@icloud.com>
2020-08-15 18:37:07 -0500

    added current day and dateto the heading

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 console.log("The current day is ");
 
+const currentDay = moment().format('dddd');
+const currentDate = moment().format("MMM Do");
+$("#currentDay").text(currentDay + ", " + currentDate);
+
       
    
