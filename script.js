84bf7372d60dd8d2ffcc95db3658aabdc851da30
Jason Perez <jasandper@icloud.com>
2020-08-16 01:29:31 -0500

    set hour block background colors to reflect past, present, and future, events.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 
-console.log("The current day is ");
-
+// Display the Current Day and Date in the top heading
 const currentDay = moment().format('dddd');
 const currentDate = moment().format("MMM Do");
 $("#currentDay").text(currentDay + ", " + currentDate);
 
+// Set hour color background 
+const currentTime = moment().format('h');
+const workDayHours = [9,10,11,12,1,2,3,4,5];
+const currentTimeIndex = workDayHours.indexOf(currentTime);
+
+$("#" + currentTime).addClass("present");
+
+for(i=currentTimeIndex-1; i>=0; i--) {
+    $("#"+ workDayHours[i]).addClass("past");
+}
+
+for(i=currentTimeIndex+1; i<=workDayHours.length; i++) {
+    $("#"+ workDayHours[i]).addClass("future");
+}
+
+console.log("Current hour is", currentTime);
       
    
